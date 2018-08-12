4340570d7eebd235f3da7fc033073bae5ac7a44f
Ross Allen <r.dallen.dev@gmail.com>
2018-08-12 15:26:42 -0600

    Added methods to switch between windows

---
+++ Game/src/UI/MainMenuController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author ross
+ */
+public class MainMenuController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    /**
+     * Go to War Screen
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    private void goToWarScreen(ActionEvent event) throws IOException{
+        Parent screen2Parent = FXMLLoader.load(getClass().getResource("War.fxml"));
+        Scene screen2Scene = new Scene(screen2Parent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(screen2Scene);
+        window.show();
+    }
+    
+    /**
+     * Go to Garbage Screen
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    private void goToGarbageScreen(ActionEvent event) throws IOException{
+        Parent screen2Parent = FXMLLoader.load(getClass().getResource("Garbage.fxml"));
+        Scene screen2Scene = new Scene(screen2Parent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(screen2Scene);
+        window.show();
+    }
+        
+        
+    
+}
